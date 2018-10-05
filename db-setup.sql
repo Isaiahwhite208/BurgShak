8bf740d62aa19303126d9ad65218c01560abdff8
Isaiah White <isaiahwhite208@gmail.com>
2018-10-05 11:17:59 -0600

    push

---
+++ db-setup.sql
@@ -0,0 +1,23 @@
+-- CREATE TABLE smoothies (
+--   id int NOT NULL AUTO_INCREMENT,
+--   name VARCHAR(255) NOT NULL,
+--   description VARCHAR(255) NOT NULL,
+--   price DECIMAL(10,2) NOT NULL,
+--   PRIMARY KEY(id)
+-- );
+
+-- INSERT INTO smoothies (name, description, price) 
+-- VALUES ("The Plain Jane", "It is just ice cream", 7.99);
+
+-- SELECT * FROM burgers;
+
+-- ALTER TABLE burgers MODIFY COLUMN price DECIMAL(10,2);
+
+-- UPDATE burgers SET 
+--   price = 7.99, 
+--   name = "The Plain Jane with Cheese", 
+--   description = "Burger on a bun with cheese"
+--   WHERE id = 1;
+
+-- DELETE FROM burgers WHERE id = 1;
+
